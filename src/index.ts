2947c16f91438fc33b668ab4f0d62ff38c10b0fb
u1aryz <u1aryz.d@gmail.com>
2022-03-05 23:09:46 +0900

    @envelop/generic-authを導入

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { createServer, useLogger } from '@graphql-yoga/node'
+import { createServer, EnvelopError, useLogger } from '@graphql-yoga/node'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { typeDefs } from './api/graphql/typeDefs'
+import { useGenericAuth } from '@envelop/generic-auth'
 
 const resolvers = {
   Query: {
@@ -13,7 +14,16 @@ const schema = makeExecutableSchema({
   typeDefs: [typeDefs],
 })
 
-const server = createServer({ schema, plugins: [useLogger()] })
+const resolveUserFn = async (context: { request: { headers: Headers } }) => {
+  const auth = context.request.headers.get('authorization')
+  // TODO
+  if (!auth) {
+    throw new EnvelopError('requires authentication.', { code: 'NOT_AUTHENTICATED' })
+  }
+  return 'user'
+}
+
+const server = createServer({ schema, plugins: [useGenericAuth({ resolveUserFn, mode: 'protect-all' }), useLogger()] })
 
 // noinspection JSIgnoredPromiseFromCall
 server.start()
