6d48b5ea8b106403838071b6e4b639340d824695
u1aryz <u1aryz.d@gmail.com>
2022-03-05 18:07:02 +0900

    graphql-yogaを追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import { createServer } from '@graphql-yoga/node'
+
+const server = createServer({
+  schema: {
+    typeDefs: `
+      type Query {
+        ping: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        ping: () => 'pong',
+      },
+    },
+  },
+})
+
+// noinspection JSIgnoredPromiseFromCall
+server.start()
